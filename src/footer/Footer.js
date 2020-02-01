19be1f9272dc39e4c81497dfed959d86df06e7f2
Nikolay Vetrov <energok3@gmail.com>
2020-02-01 18:59:09 +0200

    React App connected modules

---
+++ src/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>Copyright</footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <main>
+                <article>
+                    <header>
+                        <h1>Article 1</h1>
+                        <h2>Article 2</h2>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium asperiores ut eius dolores corrupti nisi est assumenda consequuntur dignissimos, iusto ad debitis deleniti, eos dolorum ducimus magni non ratione?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium asperiores ut eius dolores corrupti nisi est assumenda consequuntur dignissimos, iusto ad debitis deleniti, eos dolorum ducimus magni non ratione?</p>
+                </article>
+            </main>
+        );
+    }
+}
+
+export default Main;
